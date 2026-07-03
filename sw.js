811c38f481e463be3edc19735b0d1a40913ec8c8
vivan36-lang-ling <vivanny6720@gmail.com>
2026-07-03 03:35:23 -0700

    Add PWA support (manifest + service worker + icon)

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function() {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
